021e0d725e993c2762cccb549185155e01d81f99
arutonee <arutonee@proton.me>
2026-02-08 10:30:28 -0500

    removed redundant parenthesis

--- General/Expression.hs
+++ General/Expression.hs
@@ -30,8 +30,8 @@ alphabetiseVariablesInTerm (Term a xs) = Term a (sort xs)
 
 addTerm :: Term -> Term -> Term
 (Term a xs) `addTerm` (Term b ys)
-    | (Term a xs) == zeroTerm = (Term b ys)
-    | (Term b ys) == zeroTerm = (Term a xs)
+    | Term a xs == zeroTerm = Term b ys
+    | Term b ys == zeroTerm = Term a xs
     | xs == ys = Term (a + b) xs
     | otherwise = zeroTerm
   where
@@ -47,16 +47,16 @@ getRepeatedVariables :: Expression -> [[String]]
 getRepeatedVariables (Expression xs) = nub (variablesIncludingRepeats \\ variablesExcludingRepeats)
   where
     variablesExcludingRepeats = getAllVariables (Expression xs)
-    variablesIncludingRepeats = [getVariable (x) | x <- xs]
+    variablesIncludingRepeats = [getVariable x | x <- xs]
 
 getAllVariables :: Expression -> [[String]]
-getAllVariables (Expression xs) = nub [getVariable (x) | x <- xs]
+getAllVariables (Expression xs) = nub [getVariable x | x <- xs]
 
 alphabetiseVariablesInExpression :: Expression -> Expression
 alphabetiseVariablesInExpression (Expression xs) = Expression [alphabetiseVariablesInTerm x | x <- xs]
 
 addExpressionWithoutSimplifying :: Expression -> Expression -> Expression
-addExpressionWithoutSimplifying (Expression xs) (Expression ys) = Expression ((xs) ++ (ys))
+addExpressionWithoutSimplifying (Expression xs) (Expression ys) = Expression (xs ++ ys)
 
 findAddableTerms :: Term -> Expression -> Expression
 findAddableTerms a (Expression xs) = Expression [x | x <- xs, x `addTerm` a /= zeroTerm]
@@ -71,22 +71,22 @@ findNonAddableTerms a (Expression xs) = Expression [x | x <- xs, x `addTerm` a =
 simplifyLikeTermsWithoutZeros :: Expression -> Expression
 simplifyLikeTermsWithoutZeros (Expression []) = Expression []
 simplifyLikeTermsWithoutZeros (Expression [term]) = Expression [term]
-simplifyLikeTermsWithoutZeros (Expression (firstTerm : xs)) = Expression [(foldl addTerm firstTerm matchingTerms)] `addExpressionWithoutSimplifying` simplifyLikeTermsWithoutZeros (Expression nonMatchingTerms)
+simplifyLikeTermsWithoutZeros (Expression (firstTerm : xs)) = Expression [foldl addTerm firstTerm matchingTerms] `addExpressionWithoutSimplifying` simplifyLikeTermsWithoutZeros (Expression nonMatchingTerms)
   where
     zeroTerm = Term 0 []
     matchingTerms = [x | x <- xs, x `addTerm` firstTerm /= zeroTerm]
     nonMatchingTerms = xs \\ matchingTerms
 
 simplifyZeroTerms :: Expression -> Expression
-simplifyZeroTerms (Expression xs) = Expression ([x | x <- xs, getCoefficient (x) /= 0])
+simplifyZeroTerms (Expression xs) = Expression ([x | x <- xs, getCoefficient x /= 0])
   where
     zeroTerm = Term 0 []
 
 simplifyLikeTerms :: Expression -> Expression
-simplifyLikeTerms a = simplifyZeroTerms (simplifyLikeTermsWithoutZeros (alphabetiseVariablesInExpression (simplifyZeroTerms (a))))
+simplifyLikeTerms a = simplifyZeroTerms (simplifyLikeTermsWithoutZeros (alphabetiseVariablesInExpression (simplifyZeroTerms a)))
 
 addExpression :: Expression -> Expression -> Expression
-addExpression a b = simplifyLikeTerms (simplifyLikeTerms (a) `addExpressionWithoutSimplifying` simplifyLikeTerms (b))
+addExpression a b = simplifyLikeTerms (simplifyLikeTerms a `addExpressionWithoutSimplifying` simplifyLikeTerms b)
 
 scalarMultiplyExpression :: Int -> Expression -> Expression
 scalarMultiplyExpression a (Expression xs) = simplifyZeroTerms (Expression [scalarMultiplyTerm a x | x <- xs])
@@ -95,7 +95,7 @@ multiplyExpression :: Expression -> Expression -> Expression
 multiplyExpression (Expression xs) (Expression ys) = simplifyLikeTerms (Expression [x `multiplyTerm` y | x <- xs, y <- ys])
 
 instance Show Term where
-    show (Term coeff vars) = show coeff ++ intercalate "" ["(" ++ (x) ++ ")" | x <- vars]
+    show (Term coeff vars) = show coeff ++ intercalate "" ["(" ++ x ++ ")" | x <- vars]
 
 instance Show Expression where
     show (Expression xs) = intercalate " + " [show x | x <- xs]
