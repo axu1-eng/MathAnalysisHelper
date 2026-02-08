520e385a8e690731aa8e5df10d05f344e8325979
axu1-eng <axu1@haverford.edu>
2026-02-07 22:17:39 -0500

    Added Addition and Multiplication of Expressions

--- General/Expression.hs
+++ General/Expression.hs
@@ -2,10 +2,19 @@ module General.Expression (
 Term(..),
 Expression(..),
 addTerm,
-addLikeTermsInExpression,
+alphabetiseVariablesInTerm,
+alphabetiseVariablesInExpression,
+getRepeatedVariables,
+getAllVariables,
+findAddableTerms,
+findNonAddableTerms,
+simplifyLikeTerms,
+addExpression,
+scalarMultiplyExpression,
+multiplyExpression,
 ) where
 
-import Data.List (intercalate)
+import Data.List
 
 data Term = Term {coefficient :: Int, variable :: [String]} deriving (Eq)
 data Expression = Expression [Term]
@@ -13,14 +22,75 @@ data Expression = Expression [Term]
 getVariable :: Term -> [String]
 getVariable (Term a xs) = xs
 
+getCoefficient :: Term -> Int
+getCoefficient (Term a xs) = a
+
+alphabetiseVariablesInTerm :: Term -> Term
+alphabetiseVariablesInTerm (Term a xs) = Term a (sort xs) 
+
 addTerm :: Term -> Term -> Term
 (Term a xs) `addTerm` (Term b ys) 
+    | (Term a xs) == zeroTerm = (Term b ys)
+    | (Term b ys) == zeroTerm = (Term a xs)
     | xs == ys = Term (a+b) xs
-    | otherwise = Term 0 [""]
+    | otherwise = zeroTerm 
+        where zeroTerm = Term 0 [""]
 
-addLikeTermsInExpression :: Expression -> Expression -> Expression
-(Expression xs) `addLikeTermsInExpression` (Expression ys) = Expression ([term1 `addTerm` term2 | term1 <- xs, term2 <- ys, (term1 `addTerm` term2) /= zeroTerm]) where 
-    zeroTerm = Term 0 [""]
+scalarMultiplyTerm :: Int -> Term -> Term
+scalarMultiplyTerm a (Term b xs) = Term (a*b) xs 
+
+multiplyTerm :: Term -> Term -> Term
+multiplyTerm (Term a xs) (Term b ys) = Term (a*b) (xs ++ ys)
+
+getRepeatedVariables :: Expression -> [[String]]
+getRepeatedVariables (Expression xs) = nub (variablesIncludingRepeats \\ variablesExcludingRepeats) where
+    variablesExcludingRepeats = getAllVariables(Expression xs)
+    variablesIncludingRepeats = [getVariable(x) | x <- xs]
+    
+getAllVariables :: Expression -> [[String]]
+getAllVariables (Expression xs) = nub [getVariable(x) | x <- xs]
+
+alphabetiseVariablesInExpression :: Expression -> Expression
+alphabetiseVariablesInExpression (Expression xs) = Expression [alphabetiseVariablesInTerm x | x <- xs]
+
+addExpressionWithoutSimplifying :: Expression -> Expression -> Expression
+addExpressionWithoutSimplifying (Expression xs) (Expression ys) = Expression ((xs) ++ (ys))
+
+findAddableTerms :: Term -> Expression -> Expression
+findAddableTerms a (Expression xs) = Expression [x | x <- xs, x `addTerm` a /= zeroTerm] 
+    where
+        zeroTerm = Term 0 [""]
+
+findNonAddableTerms :: Term -> Expression -> Expression 
+findNonAddableTerms a (Expression xs) = Expression [x | x <- xs, x `addTerm` a == zeroTerm] 
+    where
+        zeroTerm = Term 0 [""]
+
+simplifyLikeTermsWithoutZeros :: Expression -> Expression
+simplifyLikeTermsWithoutZeros (Expression []) = Expression []
+simplifyLikeTermsWithoutZeros (Expression [term]) = Expression [term] 
+simplifyLikeTermsWithoutZeros (Expression (firstTerm:xs)) = Expression [(foldl addTerm firstTerm matchingTerms)] `addExpressionWithoutSimplifying` simplifyLikeTermsWithoutZeros(Expression nonMatchingTerms)
+    where 
+        zeroTerm = Term 0 [""]
+        matchingTerms = [x | x <- xs, x `addTerm` firstTerm /= zeroTerm]
+        nonMatchingTerms = xs \\ matchingTerms
+
+simplifyZeroTerms :: Expression -> Expression
+simplifyZeroTerms (Expression xs) = Expression ([x | x <- xs, getCoefficient(x) /= 0])
+    where
+        zeroTerm = Term 0 [""]
+
+simplifyLikeTerms :: Expression -> Expression
+simplifyLikeTerms a = simplifyZeroTerms(simplifyLikeTermsWithoutZeros(alphabetiseVariablesInExpression(simplifyZeroTerms(a))))
+        
+addExpression :: Expression -> Expression -> Expression
+addExpression a b = simplifyLikeTerms(simplifyLikeTerms(a) `addExpressionWithoutSimplifying` simplifyLikeTerms(b))
+
+scalarMultiplyExpression :: Int -> Expression -> Expression
+scalarMultiplyExpression a (Expression xs) = simplifyZeroTerms(Expression [scalarMultiplyTerm a x | x <- xs])
+
+multiplyExpression :: Expression -> Expression -> Expression
+multiplyExpression (Expression xs) (Expression ys) = simplifyLikeTerms(Expression [x `multiplyTerm` y | x <- xs, y <- ys])
 
 instance Show Term where
     show (Term coeff vars) = show coeff ++ intercalate "" ["(" ++ (x) ++ ")" | x <- vars]
--- main.hs
+++ main.hs
@@ -2,11 +2,30 @@ import qualified General.Expression as Exp
 
 main :: IO ()
 main = do
-    let a1 = Exp.Term 2 ["a1"]
-    let a2 = Exp.Term 3 ["a1"]
-    let b1 = Exp.Term 3 ["b1"]
-    let b7 = Exp.Term 9 ["b1"]
-    let exp1 = Exp.Expression [a1,b1]
-    let exp2 = Exp.Expression [a2,b7]
-    print $ Exp.addTerm a1 a2
-    print $ Exp.addLikeTermsInExpression exp1 exp2
+    let zeroTerm = Exp.Term 0 [""]
+    let twoa1 = Exp.Term 2 ["a1"]
+    let threea1 = Exp.Term 3 ["a1"]
+    let threeb1 = Exp.Term 3 ["b1"]
+    let nineb1 = Exp.Term 9 ["b1"]
+    let tenb2 = Exp.Term 10 ["b2"]
+    let exp1 = Exp.Expression [Exp.Term 2 ["a1"],Exp.Term 3 ["a1"],Exp.Term 3 ["b1"],Exp.Term 9 ["b1"],Exp.Term 10 ["b2"]]
+    let exp2 = Exp.Expression [Exp.Term 0 ["a1"],Exp.Term 3 ["b1"],Exp.Term 3 ["a1"],Exp.Term 10 ["b2"],Exp.Term 9 ["b1"]]
+    let exp3 = Exp.Expression [Exp.Term 2 ["a1"],Exp.Term 3 ["b1"],Exp.Term 0 [""],Exp.Term 3 ["a1"],Exp.Term 10 ["b2"],Exp.Term 9 ["b1"]]
+    let exp4 = Exp.Expression [Exp.Term 0 [""],Exp.Term 2 ["a1"],Exp.Term 3 ["b1"],Exp.Term 3 ["a1"],Exp.Term 10 ["b2"],Exp.Term 9 ["b1"]]
+    let exp5 = Exp.Expression [Exp.Term 7 ["a2","a1"],Exp.Term 4 ["b1","neuf"],Exp.Term 3 ["a1","a2"],Exp.Term 0 ["a"],Exp.Term 1 ["a"]]
+    let exp6 = Exp.Expression [Exp.Term (4) ["neuf","b1"],Exp.Term 2 ["b7"]]
+    let aPlusb = Exp.Expression [Exp.Term 1 ["a"],Exp.Term 1 ["b"]]
+    let aMinusb = Exp.Expression [Exp.Term 1 ["a"],Exp.Term (-1) ["b"]]
+--    print $ Exp.addTerm twoa1 threea1
+--    print $ Exp.addTerm zeroTerm twoa1
+--    print $ Exp.addTerm twoa1 zeroTerm
+--    print $ Exp.addTerm nineb1 zeroTerm
+--    print $ Exp.getRepeatedVariables exp1
+--    print $ Exp.getAllVariables exp1
+--    print $ Exp.simplifyLikeTerms exp1
+--    print $ Exp.simplifyLikeTerms exp2
+--    print $ Exp.simplifyLikeTerms exp3
+--    print $ Exp.simplifyLikeTerms exp4
+    print $ exp5 `Exp.addExpression` exp6
+    print $ Exp.scalarMultiplyExpression 9 exp6
+    print $ aPlusb `Exp.multiplyExpression` aMinusb
